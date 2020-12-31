9811ef6b87bbbddbbc5970c7d54b6a4fd2357bf7
moncho01 <rramossh@hotmail.com>
2020-12-31 18:02:22 -0400

    subida 80 parapeteo3 dcors

--- src/components/Bitacora.js
+++ src/components/Bitacora.js
@@ -295,7 +295,7 @@ console.log('Bitacora 207 gifs:',gifs.length);
                          
                             {gifs.length > 0 ?
                             <ReactTabulator
-                            data={datax}
+                             data={gifs}
                             columns={columns}
                             tooltips={true}
                             layout={"fitData"}

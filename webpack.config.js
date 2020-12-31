d390db0899616432033ae19554cfae8b99700b61
moncho01 <rramossh@hotmail.com>
2020-12-31 01:08:43 -0400

    subida9 parapeteo dcors frontend

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+       {
+       test: /\.(jpe?g|png|PNG|gif|svg)$/i,
+        use: {
+          loader: "file-loader",
+            options: {
+              query: {
+                name:'assets/[name].[ext]'
+              }
+            }
+            }
+          },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
